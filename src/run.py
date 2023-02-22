5bceadb77c71d2598a2ea0b852f6d520db434357
NguyenManhTien <manhtien.work@gmail.com>
2023-02-22 16:14:35 +0700

    remove cache

--- src/run.py
+++ src/run.py
@@ -1,29 +1,13 @@
 import json
 from flask import Flask, request
-from flask_caching import Cache
-from img2vec_keras import Img2Vec
-
-config = {
-    "DEBUG": True,          # some Flask specific configs
-    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 300
-}
-
 app = Flask(__name__)
-# tell Flask to use the above defined config
-app.config.from_mapping(config)
-cache = Cache(app)
 
-@cache.cached(timeout=50, key_prefix='init_img2vector')
-def init_img2vector():
-  img2vec = Img2Vec()
-  return img2vec
-
+from img2vec_keras import Img2Vec
 
 @app.route('/img2vector', methods=["POST"])
 def img2vector():
   url = request.json.get('url')
-  img2vec = init_img2vector()
+  img2vec = Img2Vec()
   dense_vector = img2vec.get_vec(url)
   # json_dump = json.dumps(dense_vector)
   res = {
