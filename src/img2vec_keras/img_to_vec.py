ccf9017a8858bbfdbe74bb22ee78f9bdc196d05b
NguyenManhTien <manhtien.work@gmail.com>
2023-03-03 10:46:00 +0700

    first version

--- src/img2vec_keras/img_to_vec.py
+++ src/img2vec_keras/img_to_vec.py
@@ -9,14 +9,15 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
+_HEIGHT = 512
+_WIDTH = int(2048 / _HEIGHT)
 _IMAGE_NET_TARGET_SIZE = (224, 224)
-_RESHAPE = (512,4)
+_RESHAPE = (_HEIGHT,_WIDTH)
 
 
 class Img2Vec(object):
-
     def __init__(self):
-        
+        self.default_vector = self.get_default_vector()
         model = resnet50.ResNet50(weights='imagenet')
         # model.save("weights.h5")
         # model = load_model("weights.h5")
@@ -30,20 +31,31 @@ class Img2Vec(object):
         :param image_path: path to image on filesystem
         :returns: numpy ndarray
         """
-        image_url = tf.keras.utils.get_file('Court', origin=image_path )
-        img = image.load_img(image_url, target_size=_IMAGE_NET_TARGET_SIZE )
-        os.remove(image_url)
+        try:
+            fileName = image_path.split('/')[5]
+            image_url = tf.keras.utils.get_file('img' + fileName, origin=image_path )
+            img = image.load_img(image_url, target_size=_IMAGE_NET_TARGET_SIZE )
+            # os.remove(image_url)
 
-        # img = image.load_img(image_path, target_size=_IMAGE_NET_TARGET_SIZE)
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        x = resnet50.preprocess_input(x)
-        intermediate_output = self.intermediate_layer_model.predict(x)
-        dense_vector = intermediate_output[0]
-        dense_vector = dense_vector.reshape(_RESHAPE)
-        dense_vector = tf.reduce_mean(dense_vector, 1)
-        return dense_vector.numpy().tolist()
+            # img = image.load_img(image_path, target_size=_IMAGE_NET_TARGET_SIZE)
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = resnet50.preprocess_input(x)
+            intermediate_output = self.intermediate_layer_model.predict(x)
+            dense_vector = intermediate_output[0]
+            dense_vector = dense_vector.reshape(_RESHAPE)
+            dense_vector = tf.reduce_mean(dense_vector, 1)
+            return dense_vector.numpy().tolist()
+        except:
+            print("Error get_vec: ", image_path)
+            return self.default_vector
         # return x
-    
+
+    def get_default_vector(self):
+        vector = []
+        for i in range(0, _HEIGHT):
+            vector.append(0)
+        return vector
+        
 if __name__ == "main":
      pass    
--- src/run.py
+++ src/run.py
@@ -1,17 +1,41 @@
 import json
 from flask import Flask, request
+import concurrent.futures
+import time
 app = Flask(__name__)
 
 from img2vec_keras import Img2Vec
 
 @app.route('/img2vector', methods=["POST"])
 def img2vector():
-  url = request.json.get('url')
   img2vec = Img2Vec()
-  dense_vector = img2vec.get_vec(url)
-  # json_dump = json.dumps(dense_vector)
+  urls = request.json.get('urls')
+  urls_dic = json.loads(urls)
+  # for id in urls_dic.keys():
+  #   url = urls_dic[id]
+  #   if(url):
+  #     dense_vector = img2vec.get_vec(url)
+  #     urls_dic[id] = dense_vector
+  #   else:
+  #     urls_dic[id] = img2vec.get_default_vector()
+
+
+  # first way, using multithread
+  result = {}
+  start_time = time.perf_counter()
+  with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
+    for key, value in zip(urls_dic.keys(), executor.map(img2vec.get_vec, urls_dic.values())):
+        result.update({
+          key: value
+        })
+    # executor.map(img2vec.get_vec, urls_dic.values())
+  finish_time = time.perf_counter()
+  print("Program finished in {} seconds - using multithread".format(finish_time-start_time))
+  print("---")
+
+  # json_dump = json.dumps(urls_dic)
   res = {
-    "dense-vector": dense_vector
+    "dense_vectors": result
   }
   return res
 
