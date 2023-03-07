3d4d8a1534313b8974fa894a1e7ce5647d4dc3c2
NguyenManhTien <manhtien.work@gmail.com>
2023-03-07 15:53:04 +0700

    using tf

--- src/img2vec_keras/img_to_vec.py
+++ src/img2vec_keras/img_to_vec.py
@@ -1,29 +1,23 @@
 import os
 import tensorflow as tf
-from tensorflow.keras.applications import resnet50
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Model
-from tensorflow.keras.models import load_model
 import numpy as np
+import clip
+import traceback
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 _HEIGHT = 512
-_WIDTH = int(2048 / _HEIGHT)
 _IMAGE_NET_TARGET_SIZE = (224, 224)
-_RESHAPE = (_HEIGHT,_WIDTH)
 
 
 class Img2Vec(object):
     def __init__(self):
         self.default_vector = self.get_default_vector()
-        model = resnet50.ResNet50(weights='imagenet')
-        # model.save("weights.h5")
-        # model = load_model("weights.h5")
-        layer_name = 'avg_pool'
-        self.intermediate_layer_model = Model(inputs=model.input, 
-                                              outputs=model.get_layer(layer_name).output)
+        # self.model = TFAutoModel.from_pretrained("openai/clip-vit-base-patch32")
+        # self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        self.model, self.preprocess = clip.load('ViT-B/32')
 
 
     def get_vec(self, image_path):
@@ -36,18 +30,16 @@ class Img2Vec(object):
             image_url = tf.keras.utils.get_file('img' + fileName, origin=image_path )
             img = image.load_img(image_url, target_size=_IMAGE_NET_TARGET_SIZE )
             # os.remove(image_url)
-
-            # img = image.load_img(image_path, target_size=_IMAGE_NET_TARGET_SIZE)
-            x = image.img_to_array(img)
-            x = np.expand_dims(x, axis=0)
-            x = resnet50.preprocess_input(x)
-            intermediate_output = self.intermediate_layer_model.predict(x)
-            dense_vector = intermediate_output[0]
-            dense_vector = dense_vector.reshape(_RESHAPE)
-            dense_vector = tf.reduce_mean(dense_vector, 1)
-            return dense_vector.numpy().tolist()
-        except:
-            print("Error get_vec: ", image_path)
+            image_array = image.img_to_array(img)
+            
+            image_array = self.preprocess(image_array)
+            image_array = tf.expand_dims(image_array, axis=0)
+            embedded = self.model.encode(image_array)
+            
+            return embedded.numpy().toList()
+        except Exception as e:
+            print(f"{image_path} Error get_vec: {e}")
+            traceback.print_exc()
             return self.default_vector
         # return x
 
