4e1a6219e896a7fd2cfd32b1bf2619c8883a3f14
NguyenManhTien <manhtien.work@gmail.com>
2023-03-07 13:57:43 +0700

    using pytorch

--- src/img2vec_keras/img_to_vec.py
+++ src/img2vec_keras/img_to_vec.py
@@ -1,29 +1,17 @@
 import os
-import tensorflow as tf
-from tensorflow.keras.applications import resnet50
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Model
-from tensorflow.keras.models import load_model
-import numpy as np
+import requests
+from io import BytesIO
+from sentence_transformers import SentenceTransformer
+from PIL import Image
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 _HEIGHT = 512
-_WIDTH = int(2048 / _HEIGHT)
-_IMAGE_NET_TARGET_SIZE = (224, 224)
-_RESHAPE = (_HEIGHT,_WIDTH)
-
-
 class Img2Vec(object):
     def __init__(self):
         self.default_vector = self.get_default_vector()
-        model = resnet50.ResNet50(weights='imagenet')
-        # model.save("weights.h5")
-        # model = load_model("weights.h5")
-        layer_name = 'avg_pool'
-        self.intermediate_layer_model = Model(inputs=model.input, 
-                                              outputs=model.get_layer(layer_name).output)
+        self.img_model = SentenceTransformer('clip-ViT-B-32')
 
 
     def get_vec(self, image_path):
@@ -32,20 +20,13 @@ class Img2Vec(object):
         :returns: numpy ndarray
         """
         try:
-            fileName = image_path.split('/')[5]
-            image_url = tf.keras.utils.get_file('img' + fileName, origin=image_path )
-            img = image.load_img(image_url, target_size=_IMAGE_NET_TARGET_SIZE )
-            # os.remove(image_url)
-
-            # img = image.load_img(image_path, target_size=_IMAGE_NET_TARGET_SIZE)
-            x = image.img_to_array(img)
-            x = np.expand_dims(x, axis=0)
-            x = resnet50.preprocess_input(x)
-            intermediate_output = self.intermediate_layer_model.predict(x)
-            dense_vector = intermediate_output[0]
-            dense_vector = dense_vector.reshape(_RESHAPE)
-            dense_vector = tf.reduce_mean(dense_vector, 1)
-            return dense_vector.numpy().tolist()
+            response = requests.get(image_path)
+            if response.status_code == 200 and response.content is not None:
+                image = Image.open(BytesIO(response.content))
+                embedding = self.img_model(image)
+                return embedding.tolist()
+            else:
+                return self.default_vector
         except:
             print("Error get_vec: ", image_path)
             return self.default_vector
